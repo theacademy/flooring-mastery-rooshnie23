ad04e6bc35a25a8fa9e4b9f4fdf6ecd86f278578
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-02 23:58:59 -0500

    initiated the TaxDao and TaxDaoImpl

---
+++ src/main/java/com/mthree/flooringmastery/dao/TaxDao.java
@@ -0,0 +1,7 @@
+package com.mthree.flooringmastery.dao;
+
+import com.mthree.flooringmastery.model.Tax;
+
+public interface TaxDao {
+  Tax getTaxByState(String state);
+}
---
+++ src/main/java/com/mthree/flooringmastery/dao/TaxDaoImpl.java
@@ -0,0 +1,11 @@
+package com.mthree.flooringmastery.dao;
+
+import com.mthree.flooringmastery.model.Tax;
+
+public class TaxDaoImpl implements TaxDao {
+
+  @Override
+  public Tax getTaxByState(String state) {
+    return null;
+  }
+}
