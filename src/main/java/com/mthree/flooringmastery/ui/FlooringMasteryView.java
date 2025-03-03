a9f648af16f7132734449ae5c115febe15445185
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-02 21:41:38 -0500

    added the menu component in the view

---
+++ src/main/java/com/mthree/flooringmastery/ui/FlooringMasteryView.java
@@ -0,0 +1,21 @@
+package com.mthree.flooringmastery.ui;
+
+public class FlooringMasteryView {
+  private UserIO io;
+
+  public FlooringMasteryView(UserIO io) {
+    this.io = io;
+  }
+  public int printMenuAndGetSelection() {
+    io.print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+    io.print("<<Flooring Program>>");
+    io.print("1. Display Orders");
+    io.print("2. Add an Order");
+    io.print("3. Edit an Order");
+    io.print("4. Remove an Order");
+    io.print("5. Export All Data");
+    io.print("6. Quit");
+    io.print("*\n"+"* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+    return io.readInt("Please select from the above choices.", 1, 6);
+  }
+}
