e14db4268d7728f3ff5e2082f7c36b7904cab825
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-05 16:34:09 -0500

    commit 28: change the method signatures in the OrderDao

--- src/main/java/com/mthree/flooringmastery/dao/OrderDao.java
+++ src/main/java/com/mthree/flooringmastery/dao/OrderDao.java
@@ -2,12 +2,13 @@ package com.mthree.flooringmastery.dao;
 
 import com.mthree.flooringmastery.model.Order;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface OrderDao {
-  Order getOrderByDate(LocalDate date);
-  Order getOrder(int orderNumber);
-  void addOrder(int orderNumber, Order order);
-  void editOrder(LocalDate date);
-  void removeOrder(LocalDate date);
+  List<Order> getOrdersByDate(LocalDate date);
+  Order getOrder(String orderNumber);
+  void addOrder(String orderNumber, Order order);
+  void editOrder(String orderNumber, LocalDate date);
+  void removeOrder(String orderNumber, LocalDate date);
   void saveOrder();
 }
