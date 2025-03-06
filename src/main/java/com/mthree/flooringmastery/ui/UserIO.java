828c6b55f178d20db6a674c40f0760e7f5021827
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-06 15:44:40 -0500

    commit 35: Added methods for BigDecimal and LocalDAte

--- src/main/java/com/mthree/flooringmastery/ui/UserIO.java
+++ src/main/java/com/mthree/flooringmastery/ui/UserIO.java
@@ -1,26 +1,23 @@
 package com.mthree.flooringmastery.ui;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public interface UserIO {
 
   void print(String msg);
-
-  double readDouble(String prompt);
-
-  double readDouble(String prompt, double min, double max);
-
-  float readFloat(String prompt);
-
-  float readFloat(String prompt, float min, float max);
 
   int readInt(String prompt);
 
   int readInt(String prompt, int min, int max);
 
-  long readLong(String prompt);
-
-  long readLong(String prompt, long min, long max);
-
   String readString(String prompt);
+
+  LocalDate readDate(String prompt);
+
+  LocalDate readDateFuture(String prompt);
+
+  BigDecimal readBigDecimal(String prompt);
 }
 
