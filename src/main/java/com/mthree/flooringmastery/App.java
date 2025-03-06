33da2b1cb2950bc6e7da42249af09580197ba2d7
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-06 15:37:52 -0500

    commit 32: MVC temporary start but without Spring

--- src/main/java/com/mthree/flooringmastery/App.java
+++ src/main/java/com/mthree/flooringmastery/App.java
@@ -1,8 +1,31 @@
 package com.mthree.flooringmastery;
 
+import com.mthree.flooringmastery.controller.FlooringMasteryController;
+import com.mthree.flooringmastery.dao.OrderDao;
+import com.mthree.flooringmastery.dao.OrderDaoFileImpl;
+import com.mthree.flooringmastery.dao.ProductDao;
+import com.mthree.flooringmastery.dao.ProductDaoFileImpl;
+import com.mthree.flooringmastery.dao.TaxDao;
+import com.mthree.flooringmastery.dao.TaxDaoFileImpl;
+import com.mthree.flooringmastery.service.FlooringMasteryService;
+import com.mthree.flooringmastery.service.FlooringMasteryServiceImpl;
+import com.mthree.flooringmastery.ui.FlooringMasteryView;
+import com.mthree.flooringmastery.ui.UserIO;
+import com.mthree.flooringmastery.ui.UserIOConsoleImpl;
+
 public class App {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+     UserIO myIo = new UserIOConsoleImpl();
+     FlooringMasteryView myView = new FlooringMasteryView(myIo);
+     OrderDao myDao = new OrderDaoFileImpl();
+     TaxDao myTaxDao = new TaxDaoFileImpl();
+     ProductDao myProductDao = new ProductDaoFileImpl();
+     FlooringMasteryService myService =
+           new FlooringMasteryServiceImpl(myDao, myTaxDao, myProductDao);
+     FlooringMasteryController controller =
+           new FlooringMasteryController(myService, myView);
+     controller.run();
   }
 }
