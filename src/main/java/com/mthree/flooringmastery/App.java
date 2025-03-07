69796e61acf5975602cd1d67aa958904ec119e0d
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-07 01:40:58 -0500

    commit 50: added spring to project

--- src/main/java/com/mthree/flooringmastery/App.java
+++ src/main/java/com/mthree/flooringmastery/App.java
@@ -1,29 +1,18 @@
 package com.mthree.flooringmastery;
 
 import com.mthree.flooringmastery.controller.FlooringMasteryController;
-import com.mthree.flooringmastery.dao.OrderDao;
-import com.mthree.flooringmastery.dao.OrderDaoFileImpl;
-import com.mthree.flooringmastery.dao.ProductDao;
-import com.mthree.flooringmastery.dao.ProductDaoFileImpl;
-import com.mthree.flooringmastery.dao.TaxDao;
-import com.mthree.flooringmastery.dao.TaxDaoFileImpl;
-import com.mthree.flooringmastery.service.FlooringMasteryService;
-import com.mthree.flooringmastery.service.FlooringMasteryServiceImpl;
-import com.mthree.flooringmastery.ui.FlooringMasteryView;
-import com.mthree.flooringmastery.ui.UserIO;
-import com.mthree.flooringmastery.ui.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-     UserIO myIo = new UserIOConsoleImpl();
-     FlooringMasteryView myView = new FlooringMasteryView(myIo);
-     OrderDao myDao = new OrderDaoFileImpl();
-     TaxDao myTaxDao = new TaxDaoFileImpl();
-     ProductDao myProductDao = new ProductDaoFileImpl();
-     FlooringMasteryService myService = new FlooringMasteryServiceImpl(myDao, myTaxDao, myProductDao);
-     FlooringMasteryController controller = new FlooringMasteryController(myService, myView);
-     controller.run();
+
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    // Get the controller bean
+    FlooringMasteryController controller = ctx.getBean("controller", FlooringMasteryController.class);
+
+    // Run the application
+    controller.run();
   }
 }
